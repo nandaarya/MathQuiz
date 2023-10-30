b2bfee4959d5fc843a93f380978f06a4ee3159c3
Nanda Arya Putra <nandaarya404@gmail.com>
2023-10-30 19:22:27 +0700

    add random number

--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,70 @@
 ﻿Public Class Form1
+
+    ' Create a Random object called randomizer 
+    ' to generate random numbers.
+    Private randomizer As New Random
+
+    ' These integer variables store the numbers 
+    ' for the addition problem. 
+    Private addend1 As Integer
+    Private addend2 As Integer
+
+    ' These integer variables store the numbers 
+    ' for the subtraction problem. 
+    Private minuend As Integer
+    Private subtrahend As Integer
+
+    ' These integer variables store the numbers 
+    ' for the multiplication problem. 
+    Private multiplicand As Integer
+    Private multiplier As Integer
+
+    ' These integer variables store the numbers 
+    ' for the division problem. 
+    Private dividend As Integer
+    Private divisor As Integer
+
+    Public Sub StartTheQuiz()
+        ' Fill in the addition problem.
+        ' Generate two random numbers to add.
+        ' Store the values in the variables 'addend1' and 'addend2'.
+        addend1 = randomizer.Next(51)
+        addend2 = randomizer.Next(51)
+
+        ' Convert the two randomly generated numbers
+        ' into strings so that they can be displayed
+        ' in the label controls.
+        plusLeftLabel.Text = addend1.ToString()
+        plusRightLabel.Text = addend2.ToString()
+
+        ' 'sum' is the name of the NumericUpDown control.
+        ' This step makes sure its value is zero before
+        ' adding any values to it.
+        sum.Value = 0
+
+        ' Fill in the subtraction problem.
+        minuend = randomizer.Next(1, 101)
+        subtrahend = randomizer.Next(1, minuend)
+        minusLeftLabel.Text = minuend.ToString()
+        minusRightLabel.Text = subtrahend.ToString()
+        difference.Value = 0
+
+        ' Fill in the multiplication problem.
+        multiplicand = randomizer.Next(2, 11)
+        multiplier = randomizer.Next(2, 11)
+        timesLeftLabel.Text = multiplicand.ToString()
+        timesRightLabel.Text = multiplier.ToString()
+        product.Value = 0
+
+        ' Fill in the division problem.
+        divisor = randomizer.Next(2, 11)
+        Dim temporaryQuotient As Integer = randomizer.Next(2, 11)
+        dividend = divisor * temporaryQuotient
+        dividedLeftLabel.Text = dividend.ToString()
+        dividedRightLabel.Text = divisor.ToString()
+        quotient.Value = 0
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -21,5 +87,10 @@
 
     Private Sub difference_ValueChanged(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub startButton_Click(sender As Object, e As EventArgs) Handles startButton.Click
+        StartTheQuiz()
+        startButton.Enabled = False
     End Sub
 End Class
