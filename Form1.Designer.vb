78db8555fe4749ddabb84f3d82c8e540411ffab2
Nanda Arya Putra <nandaarya404@gmail.com>
2023-10-30 17:45:08 +0700

    Add sum control to form

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,6 +24,12 @@ Partial Class Form1
     Private Sub InitializeComponent()
         timeLabel = New Label()
         Label1 = New Label()
+        plusLeftLabel = New Label()
+        Label2 = New Label()
+        plusRightLabel = New Label()
+        Label3 = New Label()
+        sum = New NumericUpDown()
+        CType(sum, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' timeLabel
@@ -45,19 +51,78 @@ Partial Class Form1
         Label1.TabIndex = 1
         Label1.Text = "Time Left"
         ' 
+        ' plusLeftLabel
+        ' 
+        plusLeftLabel.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        plusLeftLabel.Location = New Point(50, 75)
+        plusLeftLabel.Name = "plusLeftLabel"
+        plusLeftLabel.Size = New Size(60, 50)
+        plusLeftLabel.TabIndex = 2
+        plusLeftLabel.Text = "?"
+        plusLeftLabel.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(116, 75)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(60, 50)
+        Label2.TabIndex = 3
+        Label2.Text = "+"
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' plusRightLabel
+        ' 
+        plusRightLabel.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        plusRightLabel.Location = New Point(182, 75)
+        plusRightLabel.Name = "plusRightLabel"
+        plusRightLabel.Size = New Size(60, 50)
+        plusRightLabel.TabIndex = 4
+        plusRightLabel.Text = "?"
+        plusRightLabel.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label3
+        ' 
+        Label3.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(248, 75)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(60, 50)
+        Label3.TabIndex = 5
+        Label3.Text = "="
+        Label3.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' sum
+        ' 
+        sum.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        sum.Location = New Point(314, 78)
+        sum.Name = "sum"
+        sum.Size = New Size(100, 47)
+        sum.TabIndex = 6
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(482, 353)
+        Controls.Add(sum)
+        Controls.Add(Label3)
+        Controls.Add(plusRightLabel)
+        Controls.Add(Label2)
+        Controls.Add(plusLeftLabel)
         Controls.Add(Label1)
         Controls.Add(timeLabel)
         Name = "Form1"
         Text = "Math Quiz"
+        CType(sum, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents timeLabel As Label
     Friend WithEvents Label1 As Label
+    Friend WithEvents plusLeftLabel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents plusRightLabel As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents sum As NumericUpDown
 End Class
--- Form1.vb
+++ Form1.vb
@@ -10,4 +10,8 @@
     Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
 End Class
